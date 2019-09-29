d4e3cfe357f2db0bb8592c8793cd7cdcc98d07b6
Tamer <tamerkaralurt@gmail.com>
2019-09-29 13:30:14 +0300

    AlertCount eklendi

---
+++ B3_vue_cli_ile_calismak/cli-proje/src/components/AlertCount.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="alert alert-success">
+    <button @click="count">{{ count }} kez tıklandı</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  }
+};
+</script>
