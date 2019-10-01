670da900451031abf121d4690550a308dffdf6a6
Tamer Karalürt <tamerkaralurt@gmail.com>
2019-10-02 02:31:25 +0300

    route hata yakalama tamamlandı, root alanında değişim olduğunda bunu yakalama işlemine başlandı

--- B5_routing_yapisi/routing-proje/src/router.js
+++ B5_routing_yapisi/routing-proje/src/router.js
@@ -2,31 +2,37 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import ContactUs from './views/ContactUs.vue'
+import NotFound from './views/NotFound.vue'
 
 //Vue.js nin routeri kullanabilmesi için bu tanımlamayı yapmamız gerekmektedir.
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/contactus',
-      name: 'contactus',
-      component: ContactUs
-    },
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            //üst alanda import olarak çağırmadığımız component dosyalarını bu şekilde çağırabiliriz.
+            component: () => import('./views/About.vue')
+        },
+        {
+            path: '/contactus',
+            name: 'contactus',
+            component: ContactUs
+        },
+        //Bu yapı ile üstte tanımladığımız adreslerden farklı bir adres geldiğinde, bizim belirlediğimiz componenti veya direk sayfa yönlendirmesi yaparak hatalı link yakalama işlemi yapabiliriz.
+        {
+            path: '*',
+            //redirect: '/',
+            //name: 'home',
+            component: NotFound,
+        }
+    ]
 })
