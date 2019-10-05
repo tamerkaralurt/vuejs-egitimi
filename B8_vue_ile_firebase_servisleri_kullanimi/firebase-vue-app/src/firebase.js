6c7bc04881634dd8e95d5c7bbe16723d7c8851c5
Tamer Karalürt <tamerkaralurt@gmail.com>
2019-10-05 19:04:03 +0300

    B8 - firebase projeye eklendi

---
+++ B8_vue_ile_firebase_servisleri_kullanimi/firebase-vue-app/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBhlELC5lqA6VWVVgZKfkvZc_ixBdxP7dk",
+  authDomain: "vue-egitimi-1.firebaseapp.com",
+  databaseURL: "https://vue-egitimi-1.firebaseio.com",
+  projectId: "vue-egitimi-1",
+  storageBucket: "",
+  messagingSenderId: "112913428108",
+  appId: "1:112913428108:web:742d63f0d0c074c4c395a9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- B8_vue_ile_firebase_servisleri_kullanimi/firebase-vue-app/src/main.js
+++ B8_vue_ile_firebase_servisleri_kullanimi/firebase-vue-app/src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './firebase'
 
 window.$ = require('jquery');
 import 'bootstrap/dist/js/bootstrap.min';
